40239bba28d450e495db9b8d0bc3279d5c3e65d1
Narubed Kaewserd <40911777+Narubed@users.noreply.github.com>
2022-09-12 16:36:11 +0700

    first update

---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const checkToken = require("./lib/checkToken");
+const connection = require("./config/db");
+const bodyParser = require("body-parser");
+connection();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+// const corsOptions = {
+//     origin: "*",
+//     credentials: true, //access-control-allow-credentials:true
+//     optionSuccessStatus: 200,
+//   };
+
+//   app.use(cors(corsOptions));
+
+// LOGIN
+app.use("/api/admin/nba-pos/signin", require("./routes/signin"));
+// routes
+app.use("/api/admin/nba-pos/owners", checkToken, require("./routes/owner"));
+app.use("/api/admin/nba-pos/admins", checkToken, require("./routes/admin"));
+app.use("/api/admin/nba-pos/branch", checkToken, require("./routes/branch"));
+
+const port = process.env.PORT || 9012;
+// const server = app.listen(port, console.log(`Listening on port ${port}...`));
+app.listen(port, console.log(`Listening on port ${port}...`));
---
+++ models/branch.model.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+
+const BranchSchema = new mongoose.Schema({
+  branch_owner_id: { type: String, required: true },
+  branch_name: { type: String, required: false, default: "ไม่มี" }, // ชื่อสาขา เช่น สำนักงานใหญ่ สาขาเชียงใหม่ หรือชื่อร้าน
+  branch_number: { type: String, required: false, default: "0" }, // เลขที่สาขา จะไปอยู่ในเลขที่ใบเสร็จด้วย
+  branch_status: { type: Boolean, required: false, default: true },
+  branch_image: { type: String, required: false, default: "ไม่มี" },
+  branch_phone: { type: String, required: true },
+  branch_status_vat: { type: Boolean, required: false, default: false }, // ในรายงานจะเพิ่มรายงาน vat ไหม
+  branch_vat_name: { type: String, required: false, default: "ไม่มี" }, // ชื่อผู้เสียภาษี
+  branch_vat_number: { type: String, required: false, default: "ไม่มี" }, // เลขเสียภาษี
+  branch_vat_address: { type: String, required: false, default: "ไม่มี" }, // ที่อยู่ตามเอกสารหรือที่อยู่ของสาขา
+  branch_withholding_tax: { type: Boolean, required: false, default: false }, // หัก 3% แล้วเเสดงไหม
+});
+
+BranchSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, {
+    expiresIn: "2h",
+  });
+  return token;
+};
+
+const Branch = mongoose.model("branch", BranchSchema);
+
+const validate = (data) => {
+  const schema = Joi.object({
+    branch_owner_id: Joi.string(),
+    branch_name: Joi.string().default("ไม่มี"),
+    branch_number: Joi.string().default("0"),
+    branch_status: Joi.boolean().default(true),
+    branch_image: Joi.string().default("ไม่มี"),
+    branch_phone: Joi.string(),
+    branch_status_vat: Joi.boolean().default(false),
+    branch_vat_name: Joi.string().default("ไม่มี"),
+    branch_vat_number: Joi.string().default("ไม่มี"),
+    branch_vat_address: Joi.string().default("ไม่มี"),
+    branch_withholding_tax: Joi.boolean().default(false),
+  });
+  return schema.validate(data);
+};
+
+module.exports = { Branch, validate };
